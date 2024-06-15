e9e5e97e221add3f014f40446cb7a62c9cd4b172
Sarthak5598 <sarthak5598sharma@gmail.com>
2024-06-15 23:34:49 +0530

    changed it back

--- src/local_settings.py
+++ src/local_settings.py
@@ -8,6 +8,4 @@ SLACK_BOT_TOKEN = secrets["SLACK_BOT_TOKEN"]
 PLUGINS = (
     "sammich.plugins.demo.DemoPlugin",
     "sammich.plugins.contributors.ContributorPlugin",
-    "sammich.plugins.project.ProjectPlugin",
-    "sammich.plugins.repo.RepoPlugin",
 )
